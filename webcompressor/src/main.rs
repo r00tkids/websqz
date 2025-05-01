729d993f634bfe8e89ce9ffef433ae81e1b2861f
Emil Sandstø <emil.sandsto@gmail.com>
2025-05-01 23:57:09 +0200

    Add HTML output heatmap

--- webcompressor/src/main.rs
+++ webcompressor/src/main.rs
@@ -1,3 +1,7 @@
+use std::{fs::File, io::Read};
+
+use model_finder::ModelFinder;
+
 mod coder;
 mod compressor;
 mod model;
@@ -6,5 +10,16 @@ mod stats;
 mod utils;
 
 fn main() {
-    println!("Hello, world!");
+    let mut test_data = String::new();
+    File::open("tests/ray_tracer/index.js")
+        .unwrap()
+        .read_to_string(&mut test_data)
+        .unwrap();
+
+    let test_bytes = test_data.as_bytes();
+    let mut model_finder = ModelFinder::new();
+    model_finder.learn_from(test_bytes).unwrap();
+    let model_defs = &model_finder.model_defs;
+
+    stats::StatsGenerator::gather_and_dump(test_bytes, model_defs).unwrap();
 }
---
+++ webcompressor/src/stats.rs
@@ -0,0 +1,46 @@
+use std::{
+    fs::File,
+    io::{BufWriter, Read, Write},
+};
+
+use crate::model::{LnMixerPred, ModelDef};
+use anyhow::Result;
+
+pub struct StatsGenerator {}
+
+impl StatsGenerator {
+    pub fn gather_and_dump(mut byte_stream: impl Read, model_defs: &Vec<ModelDef>) -> Result<()> {
+        const ONE_OVER_8: f64 = 1. / 8.;
+        let mut bytes = Vec::<u8>::new();
+        byte_stream.read_to_end(&mut bytes)?;
+        let mut model = LnMixerPred::new(&model_defs);
+
+        let output_file = File::create("output.html")?;
+        let mut output = BufWriter::new(output_file);
+
+        write!(output, "<html><body>")?;
+
+        for b in bytes {
+            let mut avg_pred_err_byte = 0.;
+            for i in 0..8 {
+                let prob = model.prob();
+                let bit = (b >> (7 - i)) & 1;
+                let pred_err = bit as f64 - prob;
+                avg_pred_err_byte += pred_err.abs();
+                model.update(pred_err, bit);
+            }
+
+            avg_pred_err_byte *= ONE_OVER_8;
+
+            write!(
+                output,
+                "<i style=\"background: hsl({}, 100%, 40%);\">{}</i>",
+                ((1. - avg_pred_err_byte) * 75.) as u8,
+                b as char
+            )?;
+        }
+        write!(output, "</body></html>")?;
+
+        Ok(())
+    }
+}
