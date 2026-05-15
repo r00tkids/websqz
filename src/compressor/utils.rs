453a44530d9e11970d30033106c67b66a531d418
Emil Sandstø <emil@bislab.no>
2026-05-15 22:12:20 +0200

    Make handle extreme probability (p = 0., or p = 1.)

--- src/compressor/utils.rs
+++ src/compressor/utils.rs
@@ -1,6 +1,8 @@
 pub const U24_MAX: u32 = 0xffffff;
 
+pub const PROB_EPS: f64 = 1.0 / U24_MAX as f64;
 pub fn prob_stretch(prob: f64) -> f64 {
+    let prob = prob.clamp(PROB_EPS, 1.0 - PROB_EPS);
     (prob / (1. - prob)).ln()
 }
 
--- src/web/js_source/utils.js
+++ src/web/js_source/utils.js
@@ -1,8 +1,10 @@
 let U24Max = 0xffffff;
 let U32Max = 0xffffffffn;
 let U64Max = 0xffffffffffffffffn;
+let ProbEps = 1.0 / U24_MAX;
 
 let probStretch = (prob) => {
+    prob = Math.max(ProbEps, Math.min(1. - ProbEps, prob));
     return Math.log(prob / (1. - prob));
 };
 let probSquash = (prob) => {
