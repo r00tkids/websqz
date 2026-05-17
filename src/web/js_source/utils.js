9ea48d2d4d4c39de1ab2018b5fb185ce056671c5
Emil Sandstø <emil@bislab.no>
2026-05-17 22:38:21 +0200

    Fix wrong constant name in JS runtime

--- src/web/js_source/utils.js
+++ src/web/js_source/utils.js
@@ -1,7 +1,7 @@
 let U24Max = 0xffffff;
 let U32Max = 0xffffffffn;
 let U64Max = 0xffffffffffffffffn;
-let ProbEps = 1.0 / U24_MAX;
+let ProbEps = 1.0 / U24Max;
 
 let probStretch = (prob) => {
     prob = Math.max(ProbEps, Math.min(1. - ProbEps, prob));
